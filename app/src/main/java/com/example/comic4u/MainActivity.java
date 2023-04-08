a867b9c87724450356671770402519343470ab4b
Minh nhat <minhnhat@example.com>
2023-04-09 03:05:45 +0700

    dòng 8

--- app/src/main/java/com/example/comic4u/MainActivity.java
+++ app/src/main/java/com/example/comic4u/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     Intent intent;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
