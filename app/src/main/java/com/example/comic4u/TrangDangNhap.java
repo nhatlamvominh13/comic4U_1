647d4ffaa6afdf2271ddc898408a66fbdb454876
Minh nhat <minhnhat@example.com>
2023-04-12 01:27:51 +0700

    layout

---
+++ app/src/main/java/com/example/comic4u/TrangDangNhap.java
@@ -0,0 +1,13 @@
+package com.example.comic4u;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TrangDangNhap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trang_dang_nhap);
+    }
+}
